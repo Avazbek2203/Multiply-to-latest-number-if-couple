a73e904d58a4e92bbabd894ed879f60167e645fc
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-04-10 11:40:15 +0500

    Add project files.

---
+++ Revision 1.1/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Revision_1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.Write("Enter array length  : ");
+            int input = int.Parse(Console.ReadLine());
+            int[] array = new int[input];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 10);
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine();
+
+            int couple;
+            int NotCouple;
+            for (int i = 0; i < array.Length; i++)
+            {
+                couple = 1;
+                NotCouple = 1;
+                if (array[i] % 2 == 0)
+                {
+                    couple = array[i] * array[array.Length - 1];
+                    Console.WriteLine(couple);
+                }
+                if (array[i] % 2 != 0)
+                {
+                    NotCouple = array[i] * array[0];
+                    Console.WriteLine(NotCouple);
+                }
+            }
+
+
+        }
+    }
+}
